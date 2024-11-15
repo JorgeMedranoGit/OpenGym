eb8313724882e1e01e9691929471c7b4ec680fea
Oliverradictos <167943917+Oliverradictos@users.noreply.github.com>
2024-11-15 09:43:51 -0400

    empleados

--- models/empleados.py
+++ models/empleados.py
@@ -24,7 +24,7 @@ class Rol(db.Model):
     __tablename__ = 'roles'
 
     idrol = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    descripciones = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f'<Rol {self.descripciones}>'
+        return f'<Rol {self.descripcion}>'
