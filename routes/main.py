70e2e9200324cc10a3fbaad7747df1d8a3a17370
green-dv <jip2027354@est.univalle.edu>
2024-11-21 21:00:50 -0400

    Correccion bug sessiones con roles

--- routes/main.py
+++ routes/main.py
@@ -168,7 +168,8 @@ def verificar():
             session["email"] = found_user.email
             session["usuario"] = found_user.nombre + " " +  found_user.apellido
             session["empleado_id"] = found_user.idempleado
-            session["rol"] = Rol.query.get(found_user.idrol)
+            rol = Rol.query.get(found_user.idrol)
+            session["rol"] = rol.descripcion
             flash("Inicio de sesión correcto" )
             return redirect("/")
         else:
